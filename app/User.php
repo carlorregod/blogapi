15be3ae06a0c7a42c2afdd05a0dbc784abb7d844
Carlos Orrego Luno <carlos.orrego@staff.2080.cl>
2021-08-18 12:49:40 -0400

    laravel passport instalacion

--- app/User.php
+++ app/User.php
@@ -7,12 +7,14 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Hash;
+//Laravel passport
+use Laravel\Passport\HasApiTokens;
 
 
 
 class User extends Authenticatable
 {
-    use Notifiable, ApiTrait;
+    use HasApiTokens, Notifiable, ApiTrait;
 
     /**
      * The attributes that are mass assignable.
